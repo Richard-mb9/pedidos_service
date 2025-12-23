65505e9bbde81e134a35733aeaf8d83e78dc72ad
Richard <richard_mbs@hotmail.com>
2025-12-23 01:10:03 -0300

    criacao das variaveis de ambiente

--- src/config.py
+++ src/config.py
@@ -1,11 +1,23 @@
-MQ_HOST = "localhost"
-MQ_USER = "guest"
-MQ_PASSWORD = "guest"
-MQ_PORT = "5672"
+from typing import Literal
+from decouple import config  # type: ignore
+from dotenv import load_dotenv, find_dotenv
 
 
-MONGO_HOST = "localhost"
-MONGO_USERNAME = "mongo"
-MONGO_PASSWORD = "mongo"
-MONGO_PORT = "27017"
-MONGO_DATABASE = "orders"
+ENVIRONMENT: Literal["local", "HML", "DEV", "PRD"] = config(
+    "ENVIRONMENT", default="local"
+)  # type: ignore
+dotenv = find_dotenv(f".env.{ENVIRONMENT.lower()}")
+load_dotenv(dotenv)
+
+
+MQ_HOST = config("MQ_HOST")
+MQ_USER = config("MQ_USER")
+MQ_PASSWORD = config("MQ_PASSWORD")
+MQ_PORT = config("MQ_PORT")
+
+
+MONGO_HOST = config("MONGO_HOST")
+MONGO_USERNAME = config("MONGO_USERNAME")
+MONGO_PASSWORD = config("MONGO_PASSWORD")
+MONGO_PORT = config("MONGO_PORT")
+MONGO_DATABASE = config("MONGO_DATABASE")
